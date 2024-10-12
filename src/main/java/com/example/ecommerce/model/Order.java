386bbff1f10e7fdd05f419658c572c3aeb714063
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-10-12 18:59:00 +0300

    fetch: models created.

---
+++ src/main/java/com/example/ecommerce/model/Order.java
@@ -0,0 +1,33 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+@Entity
+@Table(name ="orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderDate;
+
+    @ManyToMany
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Double totalAmount;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Set<OrderItem> orderItems;
+
+    private String status;
+}
