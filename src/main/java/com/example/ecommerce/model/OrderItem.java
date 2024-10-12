7c7742666eeb562d73d1419da126f046a6785405
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-10-12 19:03:07 +0300

    fetch: OrderItem created.

---
+++ src/main/java/com/example/ecommerce/model/OrderItem.java
@@ -0,0 +1,28 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+}
